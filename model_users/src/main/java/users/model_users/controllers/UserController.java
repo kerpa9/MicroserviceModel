3abfd3d2b07bdcb4f238f30f906afb555ad97cd1
Kevin Reyes <kerpa9@gmail.com>
2025-01-30 09:54:12 -0500

    Configure database variables

--- model_users/src/main/java/users/model_users/controllers/UserController.java
+++ model_users/src/main/java/users/model_users/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,15 @@ public class UserController {
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleted(@PathVariable Long id) {
+        Optional<User> userId = userService.byId(id);
+        if (userId.isPresent()) {
+            userService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
