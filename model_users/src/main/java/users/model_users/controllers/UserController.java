ccf1511c6c28ecf44927bb15ef004eed475ebbda
Kevin Reyes <kerpa9@gmail.com>
2025-01-31 09:29:00 -0500

    Crea user and insert into database

--- model_users/src/main/java/users/model_users/controllers/UserController.java
+++ model_users/src/main/java/users/model_users/controllers/UserController.java
@@ -12,12 +12,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import users.model_users.domail.models.User;
 import users.model_users.services.UserService;
 
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     @Autowired
@@ -43,7 +46,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
+    public ResponseEntity<User> create(@RequestBody @Valid User user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(user));
 
     }
