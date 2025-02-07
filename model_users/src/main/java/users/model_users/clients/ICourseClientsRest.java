0a3846933f68f9f5d93be631bef0bdb0c3b215b9
Kevin Reyes <kerpa9@gmail.com>
2025-02-07 16:45:01 -0500

    Comunicate microservices, local and image

--- model_users/src/main/java/users/model_users/clients/ICourseClientsRest.java
+++ model_users/src/main/java/users/model_users/clients/ICourseClientsRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "model-courses", url = "localhost:3100")
+@FeignClient(name = "model-courses", url = "host.docker.internal:3001")
 public interface ICourseClientsRest {
 
     @DeleteMapping("/delete-userC/{id}")
